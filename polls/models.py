c70c07faeecab24b80a54a7fcaee57894d2ef84a
ronaldperaza <ronaldperaza460@gmail.com>
2020-04-12 23:09:22 -0400

    end part 7

--- polls/models.py
+++ polls/models.py
@@ -4,13 +4,19 @@ from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-
+    pub_date = models.DateTimeField('fecha publicacion')
+    
     def __str__(self):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Recientemente Publicado'
+
+        
 
 
 class Choice(models.Model):
